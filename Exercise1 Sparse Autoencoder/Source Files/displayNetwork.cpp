8f5bd003c9f293b66d3e209cf3756c67dfcb72a2
George-Zhu <jizhu1023@gmail.com>
2013-12-07 23:52:44 +0800

    Update displayNetwork.cpp

--- Exercise1 Sparse Autoencoder/Source Files/displayNetwork.cpp
+++ Exercise1 Sparse Autoencoder/Source Files/displayNetwork.cpp
@@ -18,35 +18,33 @@ void displayNetwork(MatrixXd feature)
     MatrixXd network(1+(sz+1)*n, 1+(sz+1)*n);
     network = MatrixXd::Zero(1+(sz+1)*n, 1+(sz+1)*n);
     MatrixXd temp(sz, sz);
+    feature = feature.array() - feature.mean();
+    for (int i=0; i<ncol; i++)
+    {
+    	for (int j=0; j<nrow; j++)
+    	{
+            temp.data()[j] = feature(j, i);
+        }
 
-	feature = feature.array() - feature.mean();
+	network.block(1+(sz+1)*(i/n), 1+(sz+1)*(i%n), sz, sz) 
+                      = (temp.array() - feature.col(i).minCoeff())
+                        / (feature.col(i).maxCoeff()-feature.col(i).minCoeff());
+    }
 
-	for (int i=0; i<ncol; i++)
-	{
-		for (int j=0; j<nrow; j++)
-		{
-			temp.data()[j] = feature(j, i);
-		}
+    Mat image;
+    image = Mat::zeros(network.rows(), network.cols(), CV_64FC1);
+    double* pimage = (double*)(image.data);
+    for (int i=0; i<network.rows(); i++)
+    {
+        for (int j=0; j<network.cols(); j++)
+        {
+            *pimage = network(i,j);
+             pimage++;
+        }
+    }
 
-		network.block(1+(sz+1)*(i/n), 1+(sz+1)*(i%n), sz, sz) 
-					  = (temp.array() - feature.col(i).minCoeff())
-					    / (feature.col(i).maxCoeff()-feature.col(i).minCoeff());
-	}
-
-	Mat image;
-	image = Mat::zeros(network.rows(), network.cols(), CV_64FC1);
-	double* pimage = (double*)(image.data);
-	for (int i=0; i<network.rows(); i++)
-	{
-		for (int j=0; j<network.cols(); j++)
-		{
-			*pimage = network(i,j);
-			pimage++;
-		}
-	}
-
- 	namedWindow("Weight Map", 0);
- 	imshow("Weight Map", image);
-	image.convertTo(image, CV_8UC1, 255);
-	imwrite("../Weight Map.jpg", image);
+    namedWindow("Weight Map", 0);
+    imshow("Weight Map", image);
+    image.convertTo(image, CV_8UC1, 255);
+    imwrite("../Weight Map.jpg", image);
 }
